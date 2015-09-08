ecb02e05d417a3f42b6d8bb17a4e7ccd6605cd34
Daniel Chatfield <chatfielddaniel@gmail.com>
2015-09-08 14:42:02 +0100

    Simple testcase

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Test")
+}
+
+func ThisIsAFunction() bool {
+	return true
+}
+
+func AnotherFunction() bool {
+	return true
+}
---
+++ test_case.go
@@ -0,0 +1,7 @@
+package main
+
+const emoji = "🔜🔜"
+
+func test() {
+	// start writing AnotherFunction here
+}
